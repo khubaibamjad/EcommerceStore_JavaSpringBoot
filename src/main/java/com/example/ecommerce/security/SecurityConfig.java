8d5f3825b3cb87c612758ed2e442e1ceb94b5830
khubaib213 <khubaib.amjad2002@gmail.com>
2026-09-12 12:47:35 -0700

    Implemented The spring actuator and enabled the /health endpoints for the URLS health monitoring

--- src/main/java/com/example/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter,
